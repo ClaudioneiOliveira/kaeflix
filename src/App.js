0cad61f47fc6d6e7ecf533628508c04eae91918e
Claudionei Oliveira <claudioneio@matriz.sicred.com>
2020-07-27 23:59:30 -0300

    Corrige erro no App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Menu from './componentes/Menu';
+import Menu from './componentes/Menu'
 import dadosIniciais from './data/dados_iniciais.json';
 import BannerMain from './componentes/BannerMain';
 import Carousel from './componentes/Carousel';
@@ -7,32 +7,41 @@ import Footer from './componentes/Footer';
 
 function App() {
   return (
-    <div style=({ background: "#141414"})>
+    <div style={{ background: "#141414" }}>
       <Menu />
-      
+
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é front-end? Trabalhando nessa porra..."}
-        />
+        videoDescription={"Javidx9 answer this little question."}
+      />
 
       <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
-        />
+      />
 
-        <Carousel
+      <Carousel
         category={dadosIniciais.categorias[1]}
-        />
+      />
 
-        <Carousel
+      <Carousel
         category={dadosIniciais.categorias[2]}
-        />
+      />      
 
-        <Carousel
+      <Carousel
         category={dadosIniciais.categorias[3]}
-        />
+      />      
 
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
