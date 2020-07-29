318e64a477ea73958e224aca0fe2e3a2300863f7
Claudionei Oliveira <claudioneio@matriz.sicred.com>
2020-07-28 22:31:15 -0300

    Implementa a aula 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Menu from '../../componentes/Menu'
+import Menu from '../../components/Menu'
 import dadosIniciais from '../../data/dados_iniciais.json';
-import BannerMain from '../../componentes/BannerMain';
-import Carousel from '../../componentes/Carousel';
-import Footer from '../../componentes/Footer';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
